f0848300bed36c6eadb4625c6777f5737e2af55f
Fraca-a <mikael.framling@gmail.com>
2026-02-04 15:08:10 +0200

    jotain

---
+++ Tehtävät/Tunnilla tehty testi.py
@@ -0,0 +1,9 @@
+luku1 = float(input('anna eka luku'))
+luku2 = float(input('anna toinen luku'))
+
+print(luku1 + luku2)
+print(luku1 - luku2)
+print(luku1 * luku2)
+print(luku1 / luku2)
+print(luku1 * luku2)
+print(luku1 / luku2)
