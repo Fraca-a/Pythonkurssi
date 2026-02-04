d35fe00eca172f3c5db920df1da3b471fbf0013d
Fraca-a <mikael.framling@gmail.com>
2026-02-04 14:57:10 +0200

    jotain

---
+++ Tehtävät/Moduuli 2.py
@@ -0,0 +1,13 @@
+name = input('anna nimesi: ')
+
+print('Hello ' + name)
+print('Hello', name)
+print(f'Hello {name}!')
+
+age = 20
+
+print(age)
+
+print(f'Your age is: {age}')
+print(f'Your age is: {age + 10}')
+
