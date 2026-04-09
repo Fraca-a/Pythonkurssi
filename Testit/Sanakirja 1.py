34b99bd89dfcc099eecab9a5b6fad2c7c21d8173
Fraca-a <mikael.framling@gmail.com>
2026-04-09 11:16:03 +0300

    ...

--- Testit/Sanakirja 1.py
+++ Testit/Sanakirja 1.py
@@ -15,4 +15,13 @@ while True:
         yhteishinta += hedelmat[hedelma]
     else:
         print("Meillä ei valitettavasti ole tätä hedelmää varastossa.")
+        lisataanko=input("Haluako lisätä sen (Y/N):").lower()
+        if lisataanko == "y":
+            hinta =float(input(f"anna hinta {hedelma}lle:"))
+            print(f"{hedelma} on lisätty kilohinnalla {hinta}!")
+
     print("yhteishinta tilauksella on", yhteishinta, "euroa.")
+
+    print("päivitetty hinnasto: ")
+    for hedelma in hedelmat:
+        print(f"hedelmä: {hedelmat[hedelma]}")
